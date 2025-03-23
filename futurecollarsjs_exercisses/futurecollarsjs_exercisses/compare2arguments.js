2b571837d6d374fece75c719514a2004ad78e14a
paupuh <116916759+paupuh@users.noreply.github.com>
2025-03-23 20:46:01 +0100

    done

---
+++ futurecollarsjs_exercisses/futurecollarsjs_exercisses/compare2arguments.js
@@ -0,0 +1,11 @@
+function compareargs(num1, num2) {
+    if num1 > num2 {
+        return num1;
+    }
+    else if (num1 < num2) {
+        return num2;
+    }
+    else {
+        return 0;
+    }
+}
--- futurecollarsjs_exercisses/futurecollarsjs_exercisses/rectangleareacounter.js
+++
@@ -1,9 +0,0 @@
-function rectangleareacounter(a, b) {
-    return a * b;
-}
-
-let a = 4;
-let b = 6;
-let area = rectangleareacounter(a, b);
-
-console.log("Rectangle area is: " + area);
