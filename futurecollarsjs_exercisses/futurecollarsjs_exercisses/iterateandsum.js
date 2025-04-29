b35fcdabf9119a05b3e0908c3da9a49e1dd353af
EU\puhlmpau <paulina.puhlmann@tietoevry.com>
2025-04-29 16:32:57 +0200

    new changes

--- futurecollarsjs_exercisses/futurecollarsjs_exercisses/iterateandsum.js
+++
@@ -1,7 +0,0 @@
-function filterItems(array1, array2) {
-    const filterredArray1 = array1.filter(num => num > 0);
-    const filterredArray2 = array2.filter(num => num > 0);
-    return [...filterredArray1, ...filterredArray2];
-}
-
-console.log(filterItems([1, -2, 3], [1, 0]));
