8dcec0f36a7738cdb520f6b8fae9894c7796d129
EU\puhlmpau <paulina.puhlmann@tietoevry.com>
2025-04-29 16:17:51 +0200

    refactor: replace iterateAndSum function with filterItems to combine positive numbers from two arrays

--- futurecollarsjs_exercisses/futurecollarsjs_exercisses/iterateandsum.js
+++ futurecollarsjs_exercisses/futurecollarsjs_exercisses/iterateandsum.js
@@ -1,6 +1,7 @@
-function iterateAndSum(array) {
-    return array.reduce((sum, current) => sum + current, 0);
+function filterItems(array1, array2) {
+    const filterredArray1 = array1.filter(num => num > 0);
+    const filterredArray2 = array2.filter(num => num > 0);
+    return [...filterredArray1, ...filterredArray2];
 }
 
-console.log(iterateAndSum([1, 2, 3]));
-console.log(iterateAndSum([10, 10, 10, 10]));
+console.log(filterItems([1, -2, 3], [1, 0]));
